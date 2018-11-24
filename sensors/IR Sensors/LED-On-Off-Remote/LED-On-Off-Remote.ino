e9b082eb9c0740420371ff179d80b624d7b3e496
ilovebook54 <42848205+ilovebook54@users.noreply.github.com>
2018-11-24 18:35:16 -0500

    Add files via upload
    No changes

---
+++ sensors/IR Sensors/LED-On-Off-Remote/LED-On-Off-Remote.ino
@@ -0,0 +1,39 @@
+#include "IRremote.h"
+
+int RECV_PIN = 11;
+int ledPin = 13;
+
+IRrecv irrecv(RECV_PIN);
+
+decode_results results;
+
+void setup()
+{
+ pinMode(RECV_PIN, INPUT);
+ pinMode(ledPin,OUTPUT);
+  Serial.begin(9600);
+  irrecv.enableIRIn(); // Start the receiver
+}
+
+void loop() {
+if (irrecv.decode(&results)) {
+Serial.println(results.value, HEX);
+
+if (results.value == 0xA32AB931){
+
+  digitalWrite(ledPin,LOW);
+  Serial.println("Inside if");
+  
+}
+
+else if (results.value == 0x371A3C86) {
+
+  digitalWrite(ledPin,HIGH);
+  Serial.println("elif");
+  
+}
+
+irrecv.resume(); // Receive the next value
+}
+}
+
